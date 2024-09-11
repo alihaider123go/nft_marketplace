37ecccbc3fa741930129d7ebe5fee6bb0536a0de
Ali Haider <alihaider123go@gmail.com>
2024-09-11 13:18:05 +0500

    Navbar structure

--- components/NavBar/HelpCenter/HelpCenter.jsx
+++ components/NavBar/HelpCenter/HelpCenter.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import Style from "./HelpCenter.module.css"
+import Link from 'next/link'
 
 const HelpCenter = () => {
+    const helpCenter = [
+        {
+            name:"About",
+            link:"about",
+        },
+        {
+            name:"Contact Us",
+            link:"contact-us",
+        },
+        {
+            name:"Sign Up",
+            link:"sign-up",
+        },
+        {
+            name:"Sign In",
+            link:"sign-in",
+        },
+        {
+            name:"Subscription",
+            link:"subscription",
+        },
+    ]
+  
   return (
-    <div>HelpCenter</div>
+    <div className={Style.box}>
+        { helpCenter.map((el,i)=>(
+            <div key={i+1} className={Style.helpCenter}>
+                <Link href={{pathname:`${el.link}`}}>{el.name}</Link>
+            </div>
+        ))}
+    </div>
   )
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import "@/styles/globals.css";
+import { NavBar,Footer } from "@/components/componentsIndex";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+        <NavBar/>
+        <Component {...pageProps} />
+    </div>
+  )
 }
