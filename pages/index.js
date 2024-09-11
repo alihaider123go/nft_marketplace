1e4be6d18a2b40f3cdff1390955109bd68bb1b55
Ali Haider <alihaider123go@gmail.com>
2024-09-11 19:30:37 +0500

    Add BigNFT slider

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
 import Style from '../styles/index.module.css'
-import { HeroSection,Service } from '@/components/componentsIndex'
+import { HeroSection,Service,BigNFTSlider } from '@/components/componentsIndex'
 
 const index = () => {
   return (
     <div className={Style.homePage}>
         <HeroSection />
         <Service />
+        <BigNFTSlider />
     </div>
   )
 }
