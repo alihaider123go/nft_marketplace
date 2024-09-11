17f7405a9c8f6cf39e167790b94480edd919ef1f
Ali Haider <alihaider123go@gmail.com>
2024-09-11 10:56:58 +0500

    NFT marketplace frontend

---
+++ components/NavBar/NavBar.jsx
@@ -0,0 +1,18 @@
+import React,{useState,useEffect} from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import {MdNotifications} from 'react-icons/md'
+import {BsSearch} from 'react-icons/bs'
+import {CgMenuLeft,CgMenuRight} from "react-icons/cg"
+
+import Style from "./NavBar.module.css"
+import {Discover,HelpCenter,Notification,Profile,SideBar} from './index'
+
+const NavBar = () => {
+  return (
+    <div>NavBar</div>
+  )
+}
+
+export default NavBar
